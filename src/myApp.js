4f76c36b4dae07fc4c5501f5b656a4f476052fcf
krav maguy <flex4lease@gmail.com>
2021-02-11 03:19:45 -0600

    changed last param in app.use

--- src/myApp.js
+++ src/myApp.js
@@ -34,7 +34,7 @@ router.get("/", (req, res) => {
 
 
 
-app.use(`/.netlify/functions/api`, router);
+app.use(`/.netlify/functions/myApp`, router);
 
 module.exports = app;
 module.exports.handler = serverless(app);
