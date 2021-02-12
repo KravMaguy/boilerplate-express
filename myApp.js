ac96b224ac0ea19278868d9cfe9e5cd4674d4dd8
krav maguy <flex4lease@gmail.com>
2021-02-11 20:25:33 -0600

    send static file

--- myApp.js
+++ myApp.js
@@ -7,17 +7,24 @@ const path = require('path')
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  console.log('dirname: ',__dirname)
-  let myArr=__dirname.split('\\')
-  myArr.pop()
-  let newDir=myArr.join('/')
-  let StaticFileLoc='views/index.html'
-  res.sendFile(newDir+"/"+StaticFileLoc)
+  // let absolutePath = __dirname + "views/index.html"
+  // res.sendFile(absolutePath)
+  // var str = __dirname;
+  // var path = str.split("/");
+  // console.log(path);
+  // return
+  // let x = __dirname.toString()
+
+  // let y= x.split("/")
+  // .slice(0, -1).join("/")+"/"
+  // res.send(y)
+  // console.log('x :', y)
+  res.send('hi')
 });
 
 router.get("/something", (req, res) => {
   res.json({
-    hello: "somethingelse!"
+    hello: "somethingelsenew u!"
   });
 });
 
